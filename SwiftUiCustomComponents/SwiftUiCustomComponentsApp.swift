9e8c321990cd820169a665ab96eb4ae5b36c89c0
fred-bowker <fred.bowker@locovo.co>
2022-01-02 12:00:52 +0000

    Initial XCode Setup

---
+++ SwiftUiCustomComponents/SwiftUiCustomComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCustomComponentsApp.swift
+//  SwiftUiCustomComponents
+//
+//  Created by Fred Bowker on 02/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCustomComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
